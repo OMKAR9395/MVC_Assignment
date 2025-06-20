e5d385c7a30d43ed059ba45eb159d1f4cf1e3f8c
OMKAR9395 <omkarmohite85@gmail.com>
2025-06-20 16:42:14 +0530

    Assignment

---
+++ Assignment03/MVC_Application/MVC_Application/Controllers/CompanyController.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MVC_Application.Models;
+using MVC_Application.QueryFunction;
+
+namespace MVC_Application.Controllers
+{
+    public class CompanyController : Controller
+    {
+        [HttpGet]
+        public ActionResult Index()
+        {
+            CompanyQuery company = new CompanyQuery();
+            DataTable dt = company.GetCompanyDetails();
+            List<CompanyModel> listModel = new List<CompanyModel>();
+            if (dt.Rows.Count > 0)
+            {
+                foreach (DataRow dr in dt.Rows)
+                {
+                    CompanyModel companyModel = new CompanyModel();
+
+                    companyModel.Comp_No = Convert.ToInt32(dr["Comp_No"]);
+                    companyModel.Comp_Name = Convert.ToString(dr["Comp_Name"]);
+                    companyModel.Strength = Convert.ToInt32(dr["Strength"]);
+                    companyModel.Remark = Convert.ToString(dr["Remarks"]);
+
+                    listModel.Add(companyModel);
+                }
+            }
+            return View(listModel);
+        }
+        [HttpGet]
+        public ActionResult Create()
+        {
+            
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            CompanyModel companyModel = new CompanyModel();
+            companyModel.Comp_Name = Convert.ToString(collection["Comp_Name"]);
+
+            if (!string.IsNullOrWhiteSpace(collection["Stength"]))
+            {
+                companyModel.Strength = Convert.ToInt32(collection["Stength"]);
+            }
+            
+            companyModel.Remark = Convert.ToString(collection["Remark"]);
+
+            //if(companyModel.Comp_Name != null && companyModel.Comp_Name != "")
+            //{
+            //    ModelState.AddModelError("Comp_Name", "Please Enter Company");
+            //}
+            if(string.IsNullOrWhiteSpace(companyModel.Comp_Name))
+            {
+                ModelState.AddModelError("Comp_Name", "Please Enter Company");
+            }
+
+            CompanyQuery company = new CompanyQuery();
+
+            if(ModelState.IsValid)
+            {
+                int res = company.InsertData(companyModel);
+
+                if (res > 0)
+                {
+                    TempData["Messege"] = "Data Inserted Successfully";
+                    return RedirectToAction("Index");
+                }
+                else
+                {
+                    return View(companyModel);
+                }
+
+            }
+            return View(companyModel);
+
+
+        }
+        public ActionResult Edit(int?  Id = 0)
+        {
+            return View();
+        }
+
+
+        public ActionResult Delete(int? Id = 0)
+        {
+            return View();
+        }
+
+        public ActionResult Details(int? Id = 0)
+        {
+            return View();
+        }
+    }
+}
---
+++ Assignment03/MVC_Application/MVC_Application/QueryFunction/CompanyQuery.cs
@@ -0,0 +1,58 @@
+﻿using MVC_Application.ExecuteFunction;
+using MVC_Application.Models;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+
+namespace MVC_Application.QueryFunction
+{
+    public class CompanyQuery
+    {
+
+        Execute execute = new Execute();
+        public DataTable GetCompanyDetails(int Id = 0)
+        {
+            DataTable dt = new DataTable();
+            string Query = "Select * from Compony";
+
+            if (Id > 0)
+            {
+                Query += " where Comp_No=" + Id;
+            }
+            dt = execute.ExcuteDatatble(Query);
+
+            return dt;
+        }
+
+        public int InsertData(CompanyModel Model)
+        {
+            string query = "Insert Into Compony(Comp_Name,Strength,Remarks) values('" + Model.Comp_Name + "'," + Model.Strength + ",";
+            query += "'" + Model.Remark + "') If(@@Error=0) Select 1 else Select 0";
+
+            int Res = execute.Excuteint(query);
+            return Res;
+        }
+
+        public int UpdateData(CompanyModel Model)
+        {
+            string query = "Update Compony Set Strength= " + Model.Strength + ", Remarks='" + Model.Remark + "' where COMP_NO=" + Model.Comp_No + "";
+            query += " If(@@Error=0) Select 1 else Select 0";
+
+            int Res = execute.Excuteint(query);
+
+            return Res;
+        }
+
+        public int DeleteData(int Id)
+        {
+            string query = "Delete From Compony Where Comp_No=" + Id;
+            query += " If(@@Error=0) Select 1 else Select 0";
+
+            int Res = execute.Excuteint(query);
+
+            return Res;
+        }
+    }
+}
